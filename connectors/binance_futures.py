c2f36ef18d23cf9504aba7c13466db0f1a1383da
Halil_Hasmer <halilpython@gmail.com>
2022-06-29 17:35:04 +0300

    finish get_last_trade_info

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -1,4 +1,5 @@
 import json
+import pprint
 
 import requests
 import websocket
@@ -6,6 +7,7 @@ import threading
 from keys import *
 import logging
 import typing
+from models import Contract
 
 logger = logging.getLogger()
 
@@ -18,24 +20,29 @@ class BinanceFuturesClient:
             self.connection_type = "Testnet"
         else:
             self._base_url = "https://fapi.binance.com"
-            self._wss_url = "wss://fsrteam.binance.com/ws"
+            self._wss_url = "wss://fstream.binance.com/ws"
             self.connection_type = "Real Account"
 
         self.platform = "binance_futures"
         self._public_key = public_key
         self._secret_key = secret_key
-
+        self._header = {"X-MBX-APIKEY": self._secret_key}
         self.connection_trials = 0
 
-    def make_get_request(self, endpoint: str, params: dict):
+        self.maker_commission: float
+        self.taker_commission: float
+
+        self.contracts = dict()
+
+    def make_get_request(self, endpoint: str, params: dict) -> typing.Union[dict, None]:
         """
         :param endpoint: depending on request type, will be copied from documentation.
-        :param params: will be sent as a query string
+        :param params: will be sent as a query string, pass dict() if not required.
         :return:
         """
         complete_url = self._base_url + endpoint
 
-        response = requests.get(complete_url, params)
+        response = requests.get(complete_url, params, headers=self._header)
         code = response.status_code
         if code // 100 == 5:
             while self.connection_trials < 5:
@@ -69,20 +76,60 @@ class BinanceFuturesClient:
     def check_api_connection(self):
         _ = self.make_get_request("/fapi/v1/ping", dict())
         get_server_time = self.make_get_request("/fapi/v1/time", dict())
-        print(f"Binance Futures Client | API is connected. Current server time: {get_server_time['serverTime']}")
+        if get_server_time:
+            print(f"Binance Futures Client | API is connected. Current server time: {get_server_time['serverTime']}")
+        else:
+            print("Binance Futures Client | API Connection Failure. ")
 
-    def get_current_contracts(self):
+    def get_current_commissions(self):
+        return
+
+    def get_base_assets(self) -> list:
         exchange_info = self.make_get_request("/fapi/v1/exchangeInfo", dict())
         assets = [asset['asset'] for asset in exchange_info['assets']]
-        print(assets)
-        viable_symbols = []
+        return assets
+
+    def get_current_contracts(self) -> typing.Dict[str, Contract]:
+        exchange_info = self.make_get_request("/fapi/v1/exchangeInfo", dict())
+        assets = self.get_base_assets()
+        contract_dict = dict()
         for contract in exchange_info['symbols']:
-            if contract['contractType'] == "PERPETUAL" and contract['status'] == "TRADING" and contract['quoteAsset'] in assets:
-                print(contract['symbol'])
-                viable_symbols.append(contract['symbol'])
-        print(len(viable_symbols))
+            if contract['contractType'] == "PERPETUAL" and contract['status'] == "TRADING" \
+                    and contract['quoteAsset'] in assets:
+                symbol = contract['symbol']
+                contract_dict[symbol] = Contract("binance_futures", contract)
+        if contract_dict is not None:
+            return contract_dict
+
+    def get_last_trade_info(self, contract: Contract, count=1) -> dict:
+        symbol = contract.symbol
+        endpoint = "/fapi/v1/trades"
+        params = {'symbol': symbol, 'limit': count}
+        response = self.make_get_request(endpoint, params)
+        response = response[0]
+        last_trade = {"price": float(response['price']),
+                      "quantity": float(response['qty']),
+                      "time": int(response['time']),
+                      "buyer_filled": bool(response['isBuyerMaker'])}
+        return last_trade
 
 
 if __name__ == '__main__':
     binance = BinanceFuturesClient(BINANCE_TESTNET_API_PUBLIC, BINANCE_TESTNET_API_SECRET, testnet=True)
-    binance.get_current_contracts()
+    contracts = binance.get_current_contracts()
+    # print(f"platform: {contracts['BTCUSDT'].platform} | type: {type(contracts['BTCUSDT'].platform)}")
+    # print(f"symbol: {contracts['BTCUSDT'].symbol} | type: {type(contracts['BTCUSDT'].symbol)}")
+    # print(f"base_asset: {contracts['BTCUSDT'].base_asset} | type: {type(contracts['BTCUSDT'].base_asset)}")
+    # print(f"quote_asset: {contracts['BTCUSDT'].quote_asset} | type: {type(contracts['BTCUSDT'].quote_asset)}")
+    # print(f"margin_asset: {contracts['BTCUSDT'].margin_asset} | type: {type(contracts['BTCUSDT'].margin_asset)}")
+    # print(f"margin_percent: {contracts['BTCUSDT'].margin_percent} | type: {type(contracts['BTCUSDT'].margin_percent)}")
+    # print(f"price_precision: {contracts['BTCUSDT'].price_precision} | type: {type(contracts['BTCUSDT'].price_precision)}")
+    # print(f"quantity_precision: {contracts['BTCUSDT'].quantity_precision} | type: {type(contracts['BTCUSDT'].quantity_precision)}")
+    # print(f"tick_size: {contracts['BTCUSDT'].tick_size} | type: {type(contracts['BTCUSDT'].tick_size)}")
+    # print(f"lot_size: {contracts['BTCUSDT'].lot_size} | type: {type(contracts['BTCUSDT'].lot_size)}")
+    # print(f"max_order_limit: {contracts['BTCUSDT'].max_order_limit} | type: {type(contracts['BTCUSDT'].max_order_limit)}")
+    # print(f"order_types: {contracts['BTCUSDT'].order_types} | type: {type(contracts['BTCUSDT'].order_types)}")
+    # print(f"time_in_forces: {contracts['BTCUSDT'].time_in_forces} | type: {type(contracts['BTCUSDT'].time_in_forces)}")
+    pprint.pprint(binance.get_last_trade_info(contracts['BTCUSDT'], 10))
+    print("*" * 20)
+    pprint.pprint(binance.get_last_trade_info(contracts['BTCUSDT']))
--- models.py
+++ models.py
@@ -1,11 +1,33 @@
-class Contracts:
-    def __init__(self, platform, data):
-        if platform == "binance_futures":
-            self.get_binance_futures_contracts(data)
+class Contract:
+    def __init__(self, platform, contract_data):
+        self.platform = platform
+        self.symbol = str()
+        self.base_asset = str()
+        self.quote_asset = str()
+        self.margin_asset = str()
+        self.margin_percent = float()
+        self.price_precision = int()
+        self.quantity_precision = int()
+        self.tick_size = float()
+        self.lot_size = float()
+        self.max_order_limit = int()
+        self.order_types = list()
+        self.time_in_forces = list()
 
-    def get_binance_futures_contracts(self, data):
-        self.symbol = data['symbol']
-        self.base_asset = data['baseAsset']
-        self.quote_asset = data['quoteAsset']
-        self.margin_asset = data['marginAsset']
+        if self.platform == "binance_futures":
+            self.get_binance_futures_contracts(contract_data)
+
+    def get_binance_futures_contracts(self, contract_data):
+        self.symbol = contract_data['symbol']
+        self.base_asset = contract_data['baseAsset']
+        self.quote_asset = contract_data['quoteAsset']
+        self.margin_asset = contract_data['marginAsset']
+        self.margin_percent = float(contract_data['requiredMarginPercent'])
+        self.price_precision = int(contract_data['pricePrecision'])
+        self.quantity_precision = int(contract_data['quantityPrecision'])
+        self.tick_size = float(contract_data['filters'][0]['tickSize'])
+        self.lot_size = float(contract_data['filters'][1]['minQty'])
+        self.max_order_limit = int(contract_data['filters'][4]['limit'])
+        self.order_types = contract_data['orderTypes']
+        self.time_in_forces = contract_data['timeInForce']
 
